6bc8d9e4549602a1e968d6c7e18649ba97f43455
Camilo Segura <camilosegurar@gmail.com>
2020-11-17 16:05:03 -0500

    Added CORS support

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
 global.__basedir = __dirname;
 
-var router = require('./routes/index');
+const app = express();
 
-var app = express();
+app.use(cors());
+
+const router = require('./routes');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'faxes')));
 
 app.use('/', router);
 

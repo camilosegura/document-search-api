8339d329a889ea48a1c47eac2e477cdde02b45c5
Camilo Segura <camilosegurar@gmail.com>
2020-11-17 16:42:38 -0500

    Added search funcionality

--- managers/medical.manager.js
+++ managers/medical.manager.js
@@ -4,11 +4,40 @@ function getAllInformation() {
   return data;
 }
 
-function getBySearch(search) {
-  return data;
+function getInformationBySearch(search) {
+  const foundData = [];
+  const keys = ['flags', 'problems', 'allergies', 'medications'];
+
+  data.forEach((item) => {
+    let foundInItem = {};
+
+    keys.forEach((key) => {
+      if (item[key]) {
+        const foundInKey = {[key]: []};
+
+        item[key].forEach((itemInKey) => {
+          if (itemInKey.humanName.toLowerCase().includes(search.toLowerCase())) {
+            foundInKey[key].push(itemInKey);
+          }
+        });
+
+        if (foundInKey[key].length) {
+          foundInItem = {...foundInItem, ...foundInKey};
+        }
+      }
+    });
+
+    if (Object.keys(foundInItem).length) {
+      foundInItem.summary = item.summary;
+
+      foundData.push(foundInItem);
+    }
+  });
+
+  return foundData;
 }
 
 module.exports = {
   getAllInformation,
-  getBySearch,
+  getInformationBySearch,
 }
